187df0f736efae70330c2fde803cc9c52883e4dd
xvxnish <58302003+avanish-dex@users.noreply.github.com>
2023-07-03 12:44:49 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -8,23 +8,32 @@ graph={
     }
 visited=[]
 queue=[]
+path=[]
 
-def bfs(visited,graph,node):
+def bfs(visited,graph,node,target):
     visited.append(node)
     queue.append(node)
+    
 
     while queue:
         m=queue.pop(0)
+        node=m[-1]
         print(m,end=" ")
+        if target == m:
+            print("true")
+        
+             
+       
+        
 
         for neighbour in graph[m]:
             if neighbour not in visited:
                 visited.append(neighbour)
                 queue.append(neighbour)
-
-
+        if node<target:
+            print(node)
 print("following is the breadth-first search")
-bfs(visited,graph,'5')
+bfs(visited,graph,'5','4')
 
 
                 
